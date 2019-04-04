a34dd31ca968a82377b8696181059edf51a72def
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-04-04 16:08:19 +0900

    [finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fdba9deb264d427cbabe78242a0e70ffcd4c753075c74d055c3d20a8ed8665e872342fa4fe097d099e0a95895c44955997cf96141bf7a541abf0efe7b2d1e089'
